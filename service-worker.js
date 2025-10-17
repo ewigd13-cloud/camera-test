64648afe9b21640100bb9ade29b5753d956e4957
ewigd13 <ewig.d13@gmail.com>
2025-10-17 09:46:37 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,8 @@ const CACHE_NAME = 'whiteboard-photo-booth-test-v1';
 const urlsToCache = [
   self.location.origin + '/camera-test/',
   self.location.origin + '/camera-test/manifest.json',
-  self.location.origin + '/camera-test/assets/index-D8SfFAaI.js',
-  self.location.origin + '/camera-test/assets/index-C8zQd2x3.css',
+  self.location.origin + '/camera-test/assets/index-kkhXQTqV.js',
+  self.location.origin + '/camera-test/assets/index-yZjIh5FW.css',
   self.location.origin + '/camera-test/icons/icon-192.png',
   self.location.origin + '/camera-test/icons/icon-512.png',
   self.location.origin + '/camera-test/fonts/NotoSerifJP-VariableFont_wght.ttf',
